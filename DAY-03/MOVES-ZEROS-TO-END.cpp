5cc8448b24f71f4aa708c68a4181dae9b0140094
MUZAMILALISULEMAN <muzamil.ali1099@gmail.com>
2025-09-29 01:16:40 +0500

    added

--- DAY-03/MOVES-ZEROS-TO-END.cpp
+++ DAY-03/MOVES-ZEROS-TO-END.cpp
@@ -1,12 +1,29 @@
 #include<iostream>
 using namespace std;
 int main(){
-int arr[5] = {1,0,2,0,3}; 
-bool check = true;
-while(check){
+	int jump = 0;
+	int n = 9;
+int arr[n] = {0,1,0,2,0,3,0,4}; 
+int j = 1;
+int temp;
+for(int i=0;i<n;i++){
+	if(arr[i]==0){
+		for(j=i;j<n;j++){
+			if(arr[j] != 0){
+				arr[i] = arr[j];
+				arr[j] = 0;
+					
+				break;
+			}else{
+				jump++;
+			}
+		}
+		
+	}
 	
 }
-
-
+for(int i=0;i<n;i++){
+	cout<<arr[i]<<" ";
+}
 return 0;
 }
